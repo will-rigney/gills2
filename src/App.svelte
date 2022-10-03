d8be3e25618aa6b3e315c9aed18fee29bcef472d
will <william@rigney.com.au>
2022-10-03 19:25:57 +1100

    fix couple of visual bugs

--- src/App.svelte
+++ src/App.svelte
@@ -64,7 +64,7 @@
 
         <!-- this thing should be pinned to top right -->
         <div
-            class="text-yellow-300 underline inline float-right leading-10 my-md-24 m-4 z-20 md:absolute md:top-0 md:right-0 md:max-w-sm"
+            class="text-yellow-300 underline leading-10 my-md-24 m-4 z-20 md:absolute md:top-0 md:right-0 md:max-w-sm"
         >
             <p>
                 NOTICE: The <span
